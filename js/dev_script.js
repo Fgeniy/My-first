8ffc83e6cfce1593c2970fcd0616e886cc6fff45
Evgeniy <id10751@ya.ru>
2017-02-12 20:08:53 +0200

    Add files via upload

---
+++ js/dev_script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){   
+
+ var item = $('.item'),
+    slideName = $('.slide_name'),
+//     nameTrig = $('#name_trig').children().length;
+   
+     for (var i, i < slideName.length, i++){
+    if (item.hasClass('active')){
+       slideName[i].addClass('highlight');
+    }};
+    
+    console.log(nameTrig);
+}); 
